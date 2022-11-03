bb0a56179d9e377ce73dabf0b3a4ddbca9c2dc14
Young il Cho <young-ch@c5r8s7.42seoul.kr>
2022-11-03 22:00:54 +0900

    update: add About

--- src/pages/login/Landing.tsx
+++ src/pages/login/Landing.tsx
@@ -2,12 +2,18 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 import Hero from './Hero';
+import About from './About';
+
+const Container = styled.div`
+  font-family: SBAggroM;
+`
 
 const Landing = () => {
     return (
-        <>
+        <Container>
             <Hero/>
-        </>
+            <About/>
+        </Container>
     )
 };
 
