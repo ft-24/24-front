c5f96b9c7e7bbbe086ff4edf389b6402f17715e7
Young il Cho <0young@kakao.com>
2022-11-04 00:11:10 +0900

    add: add components

--- src/pages/login/Landing.tsx
+++ src/pages/login/Landing.tsx
@@ -1,19 +1,53 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { motion } from 'framer-motion';
 
+import Header from './Header';
 import Hero from './Hero';
 import About from './About';
+import Feature from './Feature'
+import Footer from './Footer';
 
-const Container = styled.div`
+const Wrapper = styled.div`
   font-family: SBAggroM;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
+const buttonShadow = css`
+  text-shadow: 0.02em 0.02em var(--dark-gray), -0.02em -0.02em var(--dark-gray)
+  , 0.02em -0.02em var(--dark-gray), -0.02em 0.02em var(--dark-gray),
+  0.03em 0.03em var(--purple), -0.03em -0.03em var(--purple)
+  , 0.03em -0.03em var(--purple), -0.03em 0.03em var(--purple);
+`
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  text-align: center;
+  ${buttonShadow}
+  padding: 0.5em;
+  font-size: 2em;
+  background: var(--yellow);
+  border-radius: 0.5em;
+  margin: 1em;
+  &:hover {
+    transform: scale(1.1, 1.1);
+  }
+  @media (max-width: 1100px) {
+    position: static;
+  }
+`;
 
 const Landing = () => {
     return (
-        <Container>
+        <Wrapper>
+            <Header/>
             <Hero/>
+            <Button>START</Button>
             <About/>
-        </Container>
+            <Feature/>
+            <Footer/>
+        </Wrapper>
     )
 };
 
