3932b36b8ce05c9ce3d3d8ad11b3a1ac36e1972c
Young il Cho <0young@kakao.com>
2022-11-04 00:09:01 +0900

    feat: Footer prototype

---
+++ src/pages/login/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	margin: 3em;
+`
+
+const Footer = () => {
+	return (
+		<Wrapper>
+			Go to Top
+		</Wrapper>
+	);
+}
+
+export default Footer;
