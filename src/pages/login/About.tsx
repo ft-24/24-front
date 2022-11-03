d0fe8e5587f8d0e450aa0d8ff83c4a9ca1cb1453
Young il Cho <0young@kakao.com>
2022-11-04 00:08:12 +0900

    update: adjust layout

--- src/pages/login/About.tsx
+++ src/pages/login/About.tsx
@@ -2,34 +2,30 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
-  &, & > *, & > ul,li, h1 {
-    background: var(--white);
-    color: black;
-  }
-  width: 100%;
+  margin: 2em 2em;
+  width: 80%;
+  background: var(--white);
+  color: black;
   display: flex;
-  justify-content: center;
-  align-items: center;
   padding: 1em;
   border-radius: 2em;
   background: var(--white);
 `;
 
 const Section = styled.div`
-  width:50%;
-  font-size: 1.5em;
   display: flex;
-  flex-direction: row;
+  width:50%;
+  font-size: 1em;
 `;
 
 const MemberSection = styled.div`
   width:50%;
-  height:100%;
+  padding: 1em;
 `
 
 const TechStackSection = styled.div`
   width:50%;
-  height:100%;
+  padding: 1em;
 `;
 
 const About = () => {
@@ -37,36 +33,55 @@ const About = () => {
         <Wrapper>
             <Section>
               <TechStackSection>
+                <div>
                 <h1>Stack</h1>
+                </div>
+                <div>
                 <ul>
                   <li>reactJS</li>
                   <li>react-router-dom</li>
                   <li>styled-component</li>
                   <li>framer-motion</li>
+                  <li>docker</li>
                 </ul>
+                </div>
               </TechStackSection>
               <MemberSection>
+                <div>
                 <h1>FrontEnd</h1>
+                </div>
+                <div>
                 <ul>
                     <li>seonhjeo</li>
                     <li>sunhkim</li>
                     <li>young-ch</li>
                 </ul>
+                </div>
               </MemberSection>
             </Section>
             <Section>
               <TechStackSection>
-              <h1>Stack</h1>
+                <div>
+                <h1>Stack</h1>
+                </div>
+                <div>
                 <ul>
                   <li>nestJS</li>
+                  <li>PostgreSQL</li>
+                  <li>docker</li>
                 </ul>
+                </div>
               </TechStackSection>
               <MemberSection>
+                <div>
                 <h1>BackEnd</h1>
+                </div>
+                <div>
                     <ul>
                         <li>chanhuil</li>
                         <li>yoahn</li>
                 </ul>
+                </div>
               </MemberSection>
             </Section>
         </Wrapper>
