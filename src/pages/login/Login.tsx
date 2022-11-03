d40ac8c16d49e399b7a0ed9c0896c7808a87fe2f
seonhjeo <hoya011196@gmail.com>
2022-11-03 16:47:17 +0900

    add: adjust global font

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import LoginButton from "./LoginButton";
+
+const LayOut = styled.div`
+  display: flex;
+  justify-items: column;
+  justify-content: center;
+  align-items: center;
+`
+const Aggro = styled.h1`
+font-family: SBAggroM;
+`
+
+function Login() {
+
+    return (
+		<LayOut>
+        <div>
+          <h1>Welcome, 나눔 스퀘어 입니다ㅋㅋ</h1>
+          <Aggro>어그로에요 ㅎㅎ</Aggro>
+        </div>
+        <LoginButton/>
+		</LayOut>
+    );
+};
+
+export default Login;
