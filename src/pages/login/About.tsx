4547e53ce4c49246d51bb19c930ec96ffb2f5780
Young il Cho <young-ch@c5r6s7.42seoul.kr>
2022-11-04 12:27:36 +0900

    feat: changing content when tab

--- src/pages/login/About.tsx
+++ src/pages/login/About.tsx
@@ -1,89 +1,142 @@
+import { useState } from 'react';
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import Tech from "./Tech";
+import Member from "./Member";
 
-const Wrapper = styled.div`
-  margin: 2em 2em;
+const Window = styled.div`
+  margin-top: 5em;
   width: 80%;
-  background: var(--white);
-  color: var(--dark-gray);
+  height: 36em;
+  border-radius: 1em;
+  background: var(--dark-gray);
+  overflow: hidden;
   display: flex;
-  padding: 1em;
-  background: var(--white);
-`;
-
-const Section = styled.div`
-  display: flex;
-  width:50%;
-  font-size: 1em;
-`;
-
-const MemberSection = styled.div`
-  width:50%;
-  padding: 1em;
+  flex-direction: column;
 `
 
-const TechStackSection = styled.div`
-  width:50%;
-  padding: 1em;
-`;
+const Navbar = styled.nav`
+  background: var(--light-gray);
+  border-radius: 1em;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  height: 15%;
+  display: flex;
+  align-items:center;
+`
+
+type tabType = {
+  active : boolean,
+}
+
+const Tab = styled.div<tabType>`
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  flex-wrap: nowrap;
+  width: 100%;
+  padding: 1.5em;
+  border-radius: 1em;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  border-bottom: 1em solid white;
+  background: ${props=>props.active ? `var(--white)` : `var(--light-gray)`};
+  color: ${props=>props.active ? `var(--light-gray)` : `var(--white)`};
+  &:hover {
+    background: var(--white);
+    color: var(--dark-gray);
+  }
+` 
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  background: var(--white);
+  display: flex;
+  padding: 5em;
+`
+
+const section = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  background: var(--white);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Stack = styled(section)`
+  justify-content: flex-end;
+`
+
+const Members = styled(section)`
+  justify-content: flex-start;
+`
+
+
+type TechsMapType = {
+  [index: string] : string[][],
+}
+
+const Techs : TechsMapType = {
+  "common" : [
+    ["typescript", "TypeScript"],
+    ["docker", "Docker"],
+    ["github", "Github"],
+    ["computer", "Some CS"],
+  ],
+  "fe" : [
+    ["react", "React"],
+    ["vite", "Vite"],
+    ["styledComponents", "Styled-Components"],
+  ],
+  "be" : [
+    ["nestjs", "NestJS"],
+    ["postgresql", "PostgreSQL"],
+  ]
+};
+
+type MemberMapType = {
+  [index:string] : string[];
+}
+
+const MembersArray : MemberMapType = {
+  "common" : [
+    "chanhuil", "seonhjeo", "sunhkim", "yoahn", "young-ch"
+  ],
+  "fe" : [
+    "seonhjeo", "sunhkim", "young-ch"
+  ],
+  "be" : [
+    "chanhuil", "yoahn"
+  ]
+}
 
 const About = () => {
+  const [tab, setTab] = useState<string>("common");
+
     return (
-        <Wrapper>
-            <Section>
-              <TechStackSection>
-                <div>
-                <h1>Stack</h1>
-                </div>
-                <div>
-                <ul>
-                  <li>reactJS</li>
-                  <li>react-router-dom</li>
-                  <li>styled-component</li>
-                  <li>framer-motion</li>
-                  <li>docker</li>
-                </ul>
-                </div>
-              </TechStackSection>
-              <MemberSection>
-                <div>
-                <h1>FrontEnd</h1>
-                </div>
-                <div>
-                <ul>
-                    <li>seonhjeo</li>
-                    <li>sunhkim</li>
-                    <li>young-ch</li>
-                </ul>
-                </div>
-              </MemberSection>
-            </Section>
-            <Section>
-              <TechStackSection>
-                <div>
-                <h1>Stack</h1>
-                </div>
-                <div>
-                <ul>
-                  <li>nestJS</li>
-                  <li>PostgreSQL</li>
-                  <li>docker</li>
-                </ul>
-                </div>
-              </TechStackSection>
-              <MemberSection>
-                <div>
-                <h1>BackEnd</h1>
-                </div>
-                <div>
-                    <ul>
-                        <li>chanhuil</li>
-                        <li>yoahn</li>
-                </ul>
-                </div>
-              </MemberSection>
-            </Section>
-        </Wrapper>
+      <Window>
+        <Navbar>
+          <Tab active={tab == "common"} onClick={()=>{setTab("common")}}>Common</Tab>
+          <Tab active={tab == "fe"} onClick={()=>{setTab("fe")}}>FrontEnd</Tab>
+          <Tab active={tab == "be"} onClick={()=>{setTab("be")}}>BackEnd</Tab>
+        </Navbar>
+        <Content>
+          <Stack>
+            {
+              Techs[tab].map((ele : string[]) => <Tech img={ele[0]} title={ele[1]}/>)
+            }
+          </Stack>
+          <Members>
+            {
+              MembersArray[tab].map((ele: string) => <Member name={ele}/>)
+            }
+          </Members>
+        </Content>
+      </Window>
     );
 }
 
