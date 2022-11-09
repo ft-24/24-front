76168210af46b6f9a332cb318598a2f670390c99
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-11-10 00:15:47 +0900

    update: sidebar motion

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
 
@@ -9,7 +10,7 @@ const HeadBar = styled.div`
   width: 100%;
   top: 0;
   height: 60px;
-  line-height: 64px;
+  line-height: 44px;
   background: var(--dark-gray);
   font-family: SBAggroM;
   font-size: 24px;
@@ -19,8 +20,9 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   vertical-align: middle;
-  width: 96%;
-  height: 96%;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
   margin: 0 auto;
 `;
 
@@ -47,6 +49,11 @@ const StyledLink = styled(Link)`
   text-decoration: none !important;
 `
 
+const variants = {
+  open: { opacity: 1, x:10, y:-10 },
+  closed: { opacity: 0, x:20, y:-10 },
+}
+
 const Header = () => {
   const [toggle, setToggle] = useState(false);
 
@@ -60,7 +67,12 @@ const Header = () => {
           <StyledLink to="/main">로 고</StyledLink>
         </Logo>
         <Friends onClick={() => setToggle(!toggle)}>친 구</Friends>
+        <motion.nav
+          animate={toggle ? "open" : "closed"}
+          variants={variants}
+        >
         { toggle ? <Sidebar /> : null }
+        </motion.nav>
       </Wrapper>
     </HeadBar>
   );
