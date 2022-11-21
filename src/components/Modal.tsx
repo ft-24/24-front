98991d0eb652b9ab817c7b42b2c2bf77456a4a0d
Young il Cho <0young@kakao.com>
2022-11-21 20:32:35 +0900

    update: reusable modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,41 +1,24 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Button from "./Button";
 
 const Wrapper = styled.div`
     position: fixed;
-    height: 100%;
     width: 100%;
+    height: 100vh;
     background: #00000084;
-    z-index: 1;
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 100;
 `
 
 const Box = styled.div`
-    width: 40%;
-    height: 30%;
-    background: var(--white);
-    color:var(--light-gray);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    z-index: 2;
 `
 
-const Modal = ({setIsModalOn} : any) => {
+const Modal = ({children, modalHandler} : any) => {
   return (
-    <Wrapper onClick={()=>{setIsModalOn(false)}}>
+    <Wrapper onClick={modalHandler}>
         <Box onClick={(e)=>{ e.stopPropagation();}}>
-            Modal
-            <div>
-            <Button.link to="/matching" onClick={()=>{
-            }}>O</Button.link>
-            <Button.button onClick={()=>{
-            setIsModalOn(false)}}>X</Button.button>
-            </div>
+          {children}
         </Box>
     </Wrapper>
   )
