5d06b7c23027de2c12bdb9bb81b22ef4fd75a11c
Young il Cho <0young@kakao.com>
2022-11-22 21:41:11 +0900

    update: updating navigate path

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -3,7 +3,7 @@ import { useLocation, Navigate } from "react-router-dom";
 const Auth = () => {
   let location = useLocation();
   const idx = location.search.indexOf("?code=");
-  if (idx == -1) return <Navigate to="/" replace={true} />;
+  if (idx == -1) return <Navigate to="/login" replace={true} />;
   else {
     const code = location.search.slice(6);
     localStorage.setItem("code", code);
