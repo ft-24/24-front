aba36806e87be5bbc56528230b91d4b514248072
young-ch <0young@kakao.com>
2022-11-23 16:08:40 +0900

    update: showing auth state

--- src/components/TFA.tsx
+++ src/components/TFA.tsx
@@ -1,85 +1,91 @@
 import { useState, useRef, useEffect } from "react";
-import styled from "styled-components"
-import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { Navigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import TFAInputForm from "./TFAInputForm";
 
 const Container = styled.div`
-	width: 25rem;
-	height: 35rem;
-	background: var(--white);
-	color: var(--dark-gray);
-	border-radius: 2rem;
-	padding: 1.5rem;
+  width: 25rem;
+  height: 35rem;
+  background: var(--white);
+  color: var(--dark-gray);
+  border-radius: 2rem;
+  padding: 1.5rem;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-evenly;
-	& > h1 {
-		display: inline-block;
-		font-weight: 800;
-		font-size: 1.5rem;
-	}
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  & > h1 {
+    display: inline-block;
+    font-weight: 800;
+    font-size: 1.5rem;
+  }
+`;
 
 const ImgWrapper = styled.div`
-	width: 10rem;
-	height: 10rem;
-	background-image: url('/src/images/2fa.png');
-	background-size: contain;
-`
+  width: 10rem;
+  height: 10rem;
+  background-image: url("/src/images/2fa.png");
+  background-size: contain;
+`;
 
 type TypeContent = {
-	[index: string]: JSX.Element,
-	Init : JSX.Element,
-	Loading : JSX.Element,
-  }
+  [index: string]: JSX.Element;
+  Init: JSX.Element;
+  Loading: JSX.Element;
+};
 
 const TFA = () => {
-	const [authState, setAuthState] = useState("Init");
-	const [userInput, setUserInput] = useState("");
-	const id = useRef("");
-	let location = useLocation();
-	const idx = location.search.indexOf("?id=");
-	// if (idx == -1) return <Navigate to="/login" replace={true} />;
-	id.current = location.search.slice(4);
-	localStorage.setItem('id', id.current);
+  const [authState, setAuthState] = useState("Init");
+  const [isAuthFail, setIsAuthFail] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const id = useRef("");
+  const location = useLocation();
 
-	const content : TypeContent = {
-		Init : <TFAInputForm setAuthState={setAuthState} setUserInput={setUserInput}/>,
-		Loading : <div>Loading</div>,
-	};
+  useEffect(()=>{
+    id.current = location.search.slice(4);
+    localStorage.setItem("id", id.current);
+  }, [])
 
-	useEffect(()=>{
-		if (userInput !== "")
-			handleSubmmit();
-	},[userInput]);
+  const content: TypeContent = {
+    Init: (
+      <TFAInputForm setAuthState={setAuthState} setUserInput={setUserInput} />
+    ),
+    Loading: <div>Loading</div>,
+  };
 
-	const handleSubmmit = async () => {
-		localStorage.setItem("2facode", userInput);
-		try {
-		  setAuthState("Loading");
-		  const response = await axios.post("http://localhost:3000/login/tfa", {
-			id: id.current,
-			code: userInput,
-		  });
-			const {token, success} = await response.data;	
-			console.log(token, success);
-		} catch (error) {
-		  console.log(error);
-		}
-	  };
+  useEffect(() => {
+    if (userInput !== "") handleSubmmit();
+  }, [userInput]);
+
+  const handleSubmmit = async () => {
+    localStorage.setItem("2facode", userInput);
+    try {
+      setAuthState("Loading");
+      const response = await axios.post("http://localhost:3000/login/tfa", {
+        id: id.current,
+        code: userInput,
+      });
+      const { token, success } = await response.data;
+      if (success) {
+        return <Navigate to={`/auth?token=${token}`} replace={true} />
+      }
+      setAuthState("Init");
+      setIsAuthFail(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-	<Container>
-		<h1>2단계 보안인증</h1>
-		<h1>{authState}</h1>
-		<ImgWrapper>
-		</ImgWrapper>
-		{content["Init"]}
-	</Container>
-  )
-}
+    <Container>
+      <h1>2단계 보안인증</h1>
+      {isAuthFail ? <h1>인증코드가 틀렸습니다.</h1> : null}
+      <ImgWrapper></ImgWrapper>
+      {content[authState]}
+    </Container>
+  );
+};
 
-export default TFA
+export default TFA;
