a55412272c8862307cdaa4d6ee522f4c1abfcc90
young-ch <0young@kakao.com>
2022-12-01 13:58:16 +0900

    feat: SocketContext

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -3,32 +3,26 @@ import { io, Socket } from "socket.io-client";
 
 type SocketContext = {
   socket: Socket | undefined;
-  id: string;
   error: string;
 };
 
 export const SocketContext = createContext<SocketContext>({
   socket: undefined,
-  id: "",
   error: "",
 });
 
 export const SocketContextProvider = ({ children }:{ children: JSX.Element }) => {
   const [socket, setSocket] = useState<Socket | undefined>();
-  const [id, setSocketId] = useState("");
   const [error, setError] = useState("");
   useEffect(() => {
     const _socket = io("http://localhost:3001");
-    setSocket(_socket);
-    _socket.on("connect", () => {setSocketId(_socket.id)});
-    _socket.on("error", (error)=>{setError(error)});
+    _socket.on("connect", () => {setSocket(_socket)});
     _socket.on("disconnect", (reason)=>{setError(reason)});
     return (() => {
-      _socket.disconnect();
-    })
+      _socket?.disconnect();})
   }, []);
   return (
-    <SocketContext.Provider value={{ socket: socket, id: id, error: error }}>
+    <SocketContext.Provider value={{ socket: socket, error: error }}>
       {children}
     </SocketContext.Provider>
   );
